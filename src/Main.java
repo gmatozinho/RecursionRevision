0f68a5b8a43a753061dea42734d48f36f9f0d113
Gustavo <gustavomatozinho@gmail.com>
2018-03-07 23:38:51 -0300

    add prod and div

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1,2,3,4));
         int result = Recursion.sum(array);
-        System.out.println("Hello World!"+result);
+        int resultProd = Recursion.prod(-5,7);
+        int resultDiv = Recursion.div(100,-10);
+        System.out.println("Hello World!"+resultProd);
     }
 }
--- src/Recursion.java
+++ src/Recursion.java
@@ -6,4 +6,22 @@ public class Recursion {
     {
         return array.size() != 0 ? array.remove(0) + sum(array) : 0;
     }
+
+    public static int prod(int times, int value){
+        if (value == 0 || times == 0) {
+            return 0;
+        } else if (times > 0) {
+            return value + prod(value, times - 1);
+        }
+        return -prod(value, -times);
+    }
+
+    public static int div(int a, int b) {
+        if (a < 0) return -div(-a, b);
+        if (b < 0) return -div(a, -b);
+        if (a < b) return 0;
+        return 1 + div(a - b, b);
+    }
+
+
 }
